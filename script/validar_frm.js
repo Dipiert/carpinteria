1782d54e111065545637cdc7bc432e50f2659d55
Dipiert <damian_rott2@hotmail.com>
2018-01-24 09:10:19 -0300

    Updating input validation

--- script/validar_frm.js
+++ script/validar_frm.js
@@ -4,7 +4,7 @@ function Validador(maximaMedida) {
   this.maximo = maximaMedida;
 }
 
-const validador = new Validador(9999);
+const validador = new Validador(99999);
 
 Validador.prototype.alertEsVacio = function (campo) {
   return (`El valor de ${campo} no puede estar vacío`);
@@ -14,16 +14,16 @@ Validador.prototype.alertCaracteresInvalidos = function (campo) {
   return (`${campo} contiene caracteres inválidos`);
 };
 
-Validador.prototype.alertMayorAlPermitido = function (campo) {
-  return (`El valor de ${campo} no puede ser mayor a ${this.maximo}`);
+Validador.prototype.alertMayorAlPermitido = function (campo, maximo) {
+  return (`El valor de ${campo} no puede ser mayor a ${maximo}`);
 };
 
 Validador.prototype.esVacio = function (campo) {
   return campo === null || campo.length === 0;
 };
 
-Validador.prototype.mayorAlPermitido = function (campo) {
-  return campo > this.maximo;
+Validador.prototype.mayorAlPermitido = function (campo, maximo) {
+  return campo > maximo;
 };
 
 Validador.prototype.contieneNoNumeros = function (campo) {
@@ -49,16 +49,16 @@ Validador.prototype.sonCamposValidos = function () {
     }
   });
 
-  const codigo = window.getElementById('in_codigo').value;
-  const es_manual = window.getElementById('is_manual').checked;
+  const codigo = window.document.getElementById('in_codigo').value;
+  const es_manual = window.document.getElementById('is_manual').checked;
   if (es_manual && this.esVacio(codigo)) {
-    alert(this.alertEsVacio('código'));
+    alert(this.alertEsVacio('Código'));
     sonValidos = false;
   } else if (this.contieneNoNumeros(codigo)) {
-    alert(this.alertCaracteresInvalidos('código'));
+    alert(this.alertCaracteresInvalidos('Código'));
     sonValidos = false;
   } else if (this.mayorAlPermitido(codigo, 999999)) {
-      alert(this.alertMayorAlPermitido('in_codigo', 999999));
+      alert(this.alertMayorAlPermitido('Código', 999999));
       sonValidos = false;
   }
   
